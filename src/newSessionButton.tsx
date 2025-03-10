8695acb79c0eededdb6d81ba3f4b0158b29d419b
julianty <alexanderjulianty@gmail.com>
2025-03-10 13:53:07 -0700

    Replace form in modal

--- src/newSessionButton.tsx
+++ src/newSessionButton.tsx
@@ -1,18 +1,23 @@
-import React from "react";
 import { useDisclosure } from "@mantine/hooks";
-import { Modal, Button } from "@mantine/core";
+import { Modal, Button, TextInput, Text, Stack } from "@mantine/core";
+import { useForm } from "@mantine/form";
 import { useSession } from "./useSession";
 import { Status } from "./types";
 function NewSessionButton() {
   const [opened, { open, close }] = useDisclosure(false);
   const { options, setOptions } = useSession();
 
-  function handleSubmitOption(event: React.FormEvent<HTMLFormElement>) {
-    event.preventDefault();
-    const formData = new FormData(event.currentTarget);
+  const form = useForm({
+    mode: "uncontrolled",
+    initialValues: {
+      name: "",
+    },
+  });
+
+  function handleSubmitOption(values: { name: string }) {
     const newOption = {
-      id: formData.get("name") + Math.random().toString(),
-      name: formData.get("name") as string,
+      id: values.name + Math.random().toString(),
+      name: values.name,
       status: "undecided" as Status,
     };
     setOptions([...options, newOption]);
@@ -20,15 +25,33 @@ function NewSessionButton() {
   return (
     <>
       <Modal opened={opened} onClose={close} title="New Session">
-        <ul>
-          {options.map((option) => {
-            return <li key={option.id}>{option.name}</li>;
-          })}
-        </ul>
-        <form onSubmit={handleSubmitOption}>
-          <input name="name" placeholder="Sushi"></input>
-          <Button type="submit">Add</Button>
-        </form>
+        {options.length === 0 && (
+          <Text>Use the form below to add options to your session</Text>
+        )}
+        <Stack>
+          <ul>
+            {options.map((option) => {
+              return <li key={option.id}>{option.name}</li>;
+            })}
+          </ul>
+          <form onSubmit={form.onSubmit(handleSubmitOption)}>
+            <TextInput
+              label="Add option"
+              placeholder="Enter an option.."
+              key={form.key("name")}
+              rightSection={<Button type="submit">Add</Button>}
+              rightSectionWidth={65}
+              {...form.getInputProps("name")}
+            />
+          </form>
+          <Button
+            disabled={options.length === 0 ? true : false}
+            onClick={close}
+            variant="outline"
+          >
+            Start
+          </Button>
+        </Stack>
       </Modal>
       <Button onClick={open}>Create new session</Button>
     </>
